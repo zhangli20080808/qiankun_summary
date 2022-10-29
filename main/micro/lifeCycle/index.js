09cac22cd6f7421a63b6c5cb31d0001f75aa90b6
zhangli20080808 <791492434@qq.com>
2022-10-29 23:10:57 +0800

    生命周期的执行

--- main/micro/lifeCycle/index.js
+++ main/micro/lifeCycle/index.js
@@ -2,10 +2,17 @@ import { findAppByRoute } from '../utils'
 import { getMainLifecycle } from '../const/mainLifeCycle'
 import { loadHtml } from '../loader'
 
+
+/**
+ * 生命周期
+ 1. 获取到上一个应用的生命周期，执行unmounted，因为需要卸载
+ 2. 获取到要跳转到的子应用，执行应用的生命周期
+ * @returns 
+ */
 export const lifecycle = async () => {
   // 获取到上一个子应用
   const prevApp = findAppByRoute(window.__ORIGIN_APP__)
-
+ 
   // 获取到要跳转到的子应用
   const nextApp = findAppByRoute(window.__CURRENT_SUB_APP__)
 
@@ -17,7 +24,7 @@ export const lifecycle = async () => {
     if (prevApp.proxy) {
       prevApp.proxy.inactive() // 将沙箱销毁
     }
-    await destoryed(prevApp)
+    await destroyed(prevApp)
   }
 
   const app = await beforeLoad(nextApp)
@@ -29,6 +36,7 @@ export const beforeLoad = async (app) => {
   await runMainLifeCycle('beforeLoad')
   app && app.beforeLoad && app.beforeLoad()
 
+  // 获取所有的html资源内容
   const subApp = await loadHtml(app) // 获取的是子应用的内容
   subApp && subApp.beforeLoad && subApp.beforeLoad()
 
@@ -44,15 +52,15 @@ export const mounted = async (app) => {
   await runMainLifeCycle('mounted')
 }
 
-export const destoryed = async (app) => {
+export const destroyed = async (app) => {
   app && app.unmount && app.unmount()
 
   // 对应的执行以下主应用的生命周期
-  await runMainLifeCycle('destoryed')
+  await runMainLifeCycle('destroyed')
 }
 
 export const runMainLifeCycle = async (type) => {
-  const mainlife = getMainLifecycle()
+  const mainLife = getMainLifecycle()
 
-  await Promise.all(mainlife[type].map(async item => await item()))
+  await Promise.all(mainLife[type].map(async item => await item()))
 }
